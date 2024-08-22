9d3b6f49fe8d378425e5124d5a00fabd683ecb63
aravinthakshan <aaravinth2005@gmail.com>
2024-08-22 18:35:19 +0530

    changed image size again

--- script.py
+++ script.py
@@ -60,7 +60,7 @@ def main():
         st.write("")
         st.write("")
         st.write("")
-        st.image("logo.png",width = 450,caption="By RESEARCH AI MRM")
+        st.image("logo.png",width = 500,caption="By RESEARCH AI MRM")
         
 if __name__ == "__main__":
     main()
